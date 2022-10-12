212b17cb5687139b2943208979c5266e361a7d37
V. Afanasev <v.o.afanasev@tinkoff.ru>
2022-10-12 22:43:43 +0300

    init commit

---
+++ src/main/kotlin/ru/narryel/hibernateinaction/HibernateInActionApplication.kt
@@ -0,0 +1,11 @@
+package ru.narryel.hibernateinaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HibernateInActionApplication
+
+fun main(args: Array<String>) {
+    runApplication<HibernateInActionApplication>(*args)
+}
---
+++ src/test/kotlin/ru/narryel/hibernateinaction/HibernateInActionApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.narryel.hibernateinaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HibernateInActionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
