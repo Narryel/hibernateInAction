29eb345de62d1ce0157e7eeaebf681123949bce1
V. Afanasev <narryel@gmail.com>
2022-11-02 01:39:43 +0300

    change mapping policy

--- src/main/kotlin/ru/narryel/hibernateinaction/mapper/PersonMapper.kt
+++ src/main/kotlin/ru/narryel/hibernateinaction/mapper/PersonMapper.kt
@@ -1,12 +1,17 @@
 package ru.narryel.hibernateinaction.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.ReportingPolicy
 import ru.narryel.hibernateinaction.domain.dto.PersonDto
 import ru.narryel.hibernateinaction.domain.dto.WorkPhoneDto
 import ru.narryel.hibernateinaction.domain.entity.Person
 import ru.narryel.hibernateinaction.domain.entity.WorkPhone
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    unmappedTargetPolicy = ReportingPolicy.ERROR,
+    unmappedSourcePolicy = ReportingPolicy.ERROR
+)
 abstract class PersonMapper {
 
     abstract fun toEntity(dto: PersonDto): Person
